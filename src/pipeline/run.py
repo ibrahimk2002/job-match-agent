d85896c3ab93629d315eb8a7a89124e359a9c695
Ibrahim <i67khan@uwaterloo.ca>
2026-04-10 02:08:42 -0400

    upd: add ingestion stage and cli output for better UX

--- src/pipeline/run.py
+++ src/pipeline/run.py
@@ -1,13 +1,21 @@
 from pipeline.extract import extract_job_data
 from pipeline.match1 import run_stage1_matching
 from pipeline.match2 import run_stage2_matching
+from pipeline.ingest import ingest_data
 from db import get_top_matches, init_db
 from utils.utils import setup_logging
 
 def run_pipeline(profile):
     init_db()
+    print("Database initialized.")
     setup_logging()
+    print("Logging configured.")
+    ingest_data()
+    print("Data ingestion completed.")
+    print("Starting data extraction...")
     extract_job_data()
-    run_stage1_matching(profile)
-    run_stage2_matching(profile)
-    return get_top_matches()
+    print("Data extraction completed.")
+    # run_stage1_matching(profile)
+    # run_stage2_matching(profile)
+    # return get_top_matches()
+    return []
